56143cd9eb454b4257a5e8928f8e068a0c9b453d
Vishnu <vishnunilambur@gmail.com>
2019-08-13 20:40:21 +0530

    updated commit

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-let profileButton = document.getElementById("view-profile-btn")
-let frontPane = document.getElementById("front-pane");
-let sidePane = document.getElementById("side-pane");
-let mainContent = document.getElementById("main-content");
+sidePaneButton = document.getElementById("toggle-side-pane-button");
+sidePane = document.getElementById("side-pane");
+mainContent = document.getElementById("main-content");
+content = document.getElementById("content");
 
-profileButton.addEventListener('click', () => {
-	if (profileButton.innerHTML == 'View Profile') {
-		profileButton.innerHTML = 'Hide Profile';
-	} else {
-		profileButton.innerHTML = 'View Profile'
-	}
-	frontPane.classList.toggle('move-front-pane');
-	sidePane.classList.toggle('display-pane');
-	mainContent.classList.toggle('display-main-content');
+sidePaneButton.addEventListener("click", () => {
+	sidePane.classList.toggle("toggle-side-pane");
+	mainContent.classList.toggle("move-main-content");
+	content.classList.toggle("display-content");
 })
