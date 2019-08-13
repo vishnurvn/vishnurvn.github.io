a7dd3f4526e9f3148f7a8930473b55f7e3a1eebb
Vishnu <vishnunilambur@gmail.com>
2019-08-13 21:52:55 +0530

    updated all files

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ sidePane = document.getElementById("side-pane");
 mainContent = document.getElementById("main-content");
 content = document.getElementById("content");
 
+navAnchors = document.querySelectorAll("#side-pane > ul > li > a");
+
 sidePaneButton.addEventListener("click", () => {
 	sidePane.classList.toggle("toggle-side-pane");
 	mainContent.classList.toggle("move-main-content");
